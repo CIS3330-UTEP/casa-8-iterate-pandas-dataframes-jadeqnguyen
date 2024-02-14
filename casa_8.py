fabce649193057517f6f4f74fd7edd77d611b3dc
jadeqnguyen <jqnguyen@miners.utep.edu>
2024-02-13 23:45:57 -0700

    Update casa_8.py

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,15 @@
+# https://www.youtube.com/watch?v=mT-2AxZLtvw&t=7s
 
+import pandas as pd
+df = pd.read_csv("big-mac-full-index.csv")
+# print(df.head())
+
+# using iterrows()
+for i,r in df.iterrows():
+    print(r[r"date"],
+          r["iso_a3"])
+# r is the row
+
+# using apply()
+print(df.apply(lambda row: row ["iso_a3"], axis = 1))
+#  axis = y-axis
